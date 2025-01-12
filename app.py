88275f07525e9fa9fa3fb5e746e9685274b44e8c
sairam751 <sairam751348@gmail.com>
2025-01-12 20:50:02 +0530

    huggingface with langchain

---
+++ app.py
@@ -0,0 +1,63 @@
+import validators,streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain.schema import Document 
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+## sstreamlit APP
+st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
+st.title("🦜 LangChain: Summarize Text From YT or Website")
+st.subheader('Summarize URL')
+
+
+
+## Get the Groq API Key and url(YT or website)to be summarized
+with st.sidebar:
+    hf_api_key=st.text_input("Huggingface API Token",value="",type="password")
+
+generic_url=st.text_input("URL",label_visibility="collapsed")
+
+## Gemma Model USsing Groq API
+##llm =ChatGroq(model="Gemma-7b-It", groq_api_key=groq_api_key)
+repo_id="mistralai/Mistral-7B-Instruct-v0.3"
+llm=HuggingFaceEndpoint(repo_id=repo_id,max_length=150,temperature=0.7,token=hf_api_key)
+
+prompt_template="""
+Provide a summary of the following content in 300 words:
+Content:{text}
+
+"""
+prompt=PromptTemplate(template=prompt_template,input_variables=["text"])
+
+if st.button("Summarize the Content from YT or Website"):
+    ## Validate all the inputs
+    if not hf_api_key.strip() or not generic_url.strip():
+        st.error("Please provide the information to get started")
+    elif not validators.url(generic_url):
+        st.error("Please enter a valid Url. It can may be a YT video utl or website url")
+
+    else:
+        try:
+            with st.spinner("Waiting..."):
+                ## loading the website or yt video data
+                if "youtube.com" in generic_url:
+                    video_id = generic_url.split("v=")[-1]
+                    transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id)
+                    text = " ".join([entry['text'] for entry in transcript])
+                    docs = [Document(page_content=text)]
+                else:
+                    loader=UnstructuredURLLoader(urls=[generic_url],mode='elements')
+                    docs=loader.load()
+                    st.text_input(label="text",value=docs[0])
+                ## Chain For Summarization
+                chain=load_summarize_chain(llm,chain_type="stuff",prompt=prompt)
+                output_summary=chain.run(docs)
+
+                st.success(output_summary)
+        except Exception as e:
+            st.exception(f"Exception:{e}")
+                    
